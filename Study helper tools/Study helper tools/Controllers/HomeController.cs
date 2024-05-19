1f539f65ff911c73ecc00830a902ebc3dd912e70
Elaraby218 <152742814+Elaraby218@users.noreply.github.com>
2024-05-19 09:29:52 +0300

    start the project

---
+++ Study helper tools/Study helper tools/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Study_helper_tools.Models;
+using System.Diagnostics;
+
+namespace Study_helper_tools.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Study helper tools/Study helper tools/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Study_helper_tools.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
