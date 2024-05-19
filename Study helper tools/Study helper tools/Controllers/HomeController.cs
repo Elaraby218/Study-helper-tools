2aa591d157cfc62806a5266c8e2082e5bdb49495
Elaraby218 <152742814+Elaraby218@users.noreply.github.com>
2024-05-19 11:46:55 +0300

    added conroller to handel signup and another one to handle login

--- Study helper tools/Study helper tools/Controllers/HomeController.cs
+++ Study helper tools/Study helper tools/Controllers/HomeController.cs
@@ -28,5 +28,7 @@ namespace Study_helper_tools.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
     }
 }
--- Study helper tools/Study helper tools/Controllers/SignUpLoginController.cs
+++ Study helper tools/Study helper tools/Controllers/SignUpLoginController.cs
@@ -1,12 +1,57 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Study_helper_tools.Database;
+using Study_helper_tools.Models;
 
 namespace Study_helper_tools.Controllers
 {
     public class SignUpLoginController : Controller
     {
+        private readonly appDbContext _context;
+        public SignUpLoginController(appDbContext context)
+        {
+            _context = context;
+        }
+
+        // index page
         public IActionResult Index()
         {
             return View();
         }
+
+        //sign up action
+        [HttpPost]
+        public IActionResult SignUp(User user)
+        {
+
+            User InUser = _context.Users.FirstOrDefault(u => u.Username == user.Username);
+            if (InUser == null)
+            {
+                user.RegisterDate = DateTime.Now;
+                _context.Users.Add(user);
+                _context.SaveChanges();
+                return RedirectToAction("Privacy", "Home");
+            }
+            else
+            {
+                return Content("User already exists");
+            }
+        }
+
+        //login action
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            User InUser = _context.Users.FirstOrDefault(u => u.Username == user.Username && u.Password == user.Password);
+            if (InUser != null)
+            {
+                return RedirectToAction("Privacy", "Home");
+            }
+            else
+            {
+                return Content("User not found");
+            }
+        }
+
+        
     }
 }
