8e9cd571d5e47d4dd1907a136a759f197c7ffcc9
Elaraby218 <152742814+Elaraby218@users.noreply.github.com>
2024-05-19 12:34:29 +0300

    new migration added to add photo path to the user

---
+++ Study helper tools/Study helper tools/Migrations/20240519093339_init1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Study_helper_tools.Migrations
+{
+    /// <inheritdoc />
+    public partial class init1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhotoPath",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhotoPath",
+                table: "Users");
+        }
+    }
+}
