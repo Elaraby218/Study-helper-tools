90254738ce9b2dcd264f84905d3cfb354cf0b8af
Elaraby218 <152742814+Elaraby218@users.noreply.github.com>
2024-05-19 10:07:12 +0300

    Models done (User and Task)

---
+++ Study helper tools/Study helper tools/Models/ToDo.cs
@@ -0,0 +1,14 @@
+﻿namespace Study_helper_tools.Models
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        public string Task { get; set; }
+        public bool IsDone { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime AddedDate { get; set; }
+        public DateTime DoneDate { get; set; }
+        public int TimePerTask { get; set; }
+        public int UserId { get; set; }
+    }
+}
