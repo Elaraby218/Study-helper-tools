60d87fb41514e25a495881d3c2eaba0c037b1375
Elaraby218 <152742814+Elaraby218@users.noreply.github.com>
2024-05-19 10:06:51 +0300

    Database connection done and create the sqllite database

---
+++ Study helper tools/Study helper tools/Database/appDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Study_helper_tools.Models;
+namespace Study_helper_tools.Database
+{
+    public class appDbContext : DbContext
+    {
+        public appDbContext(DbContextOptions<appDbContext> options) : base(options)
+        {
+        }
+        public appDbContext() { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+        { 
+            optionsBuilder.UseSqlite("Data Source=Database\\StudyHelperTools.db"); 
+        }
+        public DbSet<ToDo> ToDos { get; set; }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
