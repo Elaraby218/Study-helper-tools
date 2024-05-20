4dbee59c239cc0481aa2f79aaadaa4d8a669ab28
Elaraby218 <152742814+Elaraby218@users.noreply.github.com>
2024-05-20 17:35:06 +0300

    to do controller added

---
+++ Study helper tools/Study helper tools/Controllers/ToDoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Study_helper_tools.Models;
+
+namespace Study_helper_tools.Controllers
+{
+    public class ToDoController : Controller
+    {
+        public IActionResult ToDOIndex()
+        {
+            SharedValues.setActive("ToDo");
+            return View();
+        }
+    }
+}
