dcf6fe20ca47d5ddd10616ff88eac727d5a41767
Elaraby218 <152742814+Elaraby218@users.noreply.github.com>
2024-05-20 14:18:18 +0300

    added pomodoro controller

---
+++ Study helper tools/Study helper tools/Controllers/PromodoroController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Study_helper_tools.Models;
+
+namespace Study_helper_tools.Controllers
+{
+    public class PromodoroController : Controller
+    {
+        public IActionResult PromodoroIndex()
+        {
+            SharedValues.setActive("Promdoro"); 
+            return View();
+        }
+    }
+}
