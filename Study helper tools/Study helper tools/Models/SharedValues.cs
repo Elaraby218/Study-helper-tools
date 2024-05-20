9d74c7312a195e7eecc7bab37a84ce3fc300a1ef
Elaraby218 <152742814+Elaraby218@users.noreply.github.com>
2024-05-20 10:40:02 +0300

    class shared value added to hold important data

---
+++ Study helper tools/Study helper tools/Models/SharedValues.cs
@@ -0,0 +1,59 @@
+﻿namespace Study_helper_tools.Models
+{
+    public static class SharedValues
+    {
+        public static User CurUser = null;
+        public static string AnalyticsActive = "";
+        public static string DashBoardActive = "";
+        public static string ToDoActive = "";
+        public static string PromdoroActive = "";
+
+        public static void setCurUser(User user)
+        {
+            CurUser = user;
+        }
+
+        public static void setActive(string activeOne)
+        {
+            if(activeOne == "Analytics")
+            {
+                AnalyticsActive = "active";
+                setNull("Analytics");
+            }
+            if(activeOne == "DashBoard")
+            {
+                DashBoardActive = "active";
+                setNull("DashBoard");
+            }
+            if(activeOne == "ToDo")
+            {
+                ToDoActive = "active";
+                setNull("ToDo");
+            }
+            if(activeOne == "Promdoro")
+            {
+                PromdoroActive = "active";
+                setNull("Promdoro");
+            }
+        }
+        public static void setNull(string activeOne)
+        {
+            if(activeOne != "Analytics")
+            {
+                AnalyticsActive = "";
+            }
+            if(activeOne != "DashBoard")
+            {
+                DashBoardActive = "";
+            }
+            if(activeOne != "ToDo")
+            {
+                ToDoActive = "";
+            }
+            if(activeOne != "Promdoro")
+            {
+                PromdoroActive = "";
+            }
+        }
+    }
+}
