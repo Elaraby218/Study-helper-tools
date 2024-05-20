f10b368c46a84abfafc7af63da927337595d20f0
Elaraby218 <152742814+Elaraby218@users.noreply.github.com>
2024-05-20 23:51:31 +0300

    - add dbcontext injection - add task action added and it work as edit task also - function reset task to real time update tasks in containers - mark as done action added - delet task action added

--- Study helper tools/Study helper tools/Controllers/ToDoController.cs
+++ Study helper tools/Study helper tools/Controllers/ToDoController.cs
@@ -1,14 +1,75 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Study_helper_tools.Database;
 using Study_helper_tools.Models;
+using System.Numerics;
 
 namespace Study_helper_tools.Controllers
 {
     public class ToDoController : Controller
     {
+        private readonly appDbContext _context;
+        public ToDoController(appDbContext context) { _context = context; }
+
         public IActionResult ToDOIndex()
         {
             SharedValues.setActive("ToDo");
-            return View();
+            return View(new ToDo());
+        }
+
+        [HttpPost]
+        public IActionResult AddTask(ToDo task)
+        {
+            ToDo newTask = _context.ToDos.FirstOrDefault(t => t.Id == task.Id);
+            if(newTask==null)
+            {
+                task.AddedDate = DateTime.Now;
+                task.IsDone = false;
+                task.IsDeleted = false;
+                task.UserId = SharedValues.CurUser.Id;
+                task.TimePerTask = 0;
+                task.DoneDate = DateTime.MinValue;
+                _context.ToDos.Add(task);
+                _context.SaveChanges();
+                SharedValues.CurUserTasks.Add(task);
+                SharedValues.setTasks();
+            }
+            else
+            {
+                newTask.TaskTitle = task.TaskTitle;
+                newTask.TaskDescription = task.TaskDescription;
+                _context.SaveChanges();
+                resetTasks();
+            }
+            return RedirectToAction("ToDOIndex");
+        }
+        private void resetTasks()
+        {
+            SharedValues.CurUserTasks = _context.ToDos.Where(t => t.UserId == SharedValues.CurUser.Id && t.IsDeleted == false).ToList();
+            SharedValues.setTasks();
+        }
+        public IActionResult MarkTaskAsDone(int taskID)
+        {
+            ToDo task = _context.ToDos.FirstOrDefault(t => t.Id == taskID);
+            task.IsDone = true;
+            task.DoneDate = DateTime.Now;
+            _context.SaveChanges();
+            resetTasks();
+            return RedirectToAction("ToDOIndex");
+        }
+
+        public IActionResult DelteTask(int taskID)
+        {
+            ToDo task = _context.ToDos.FirstOrDefault(t => t.Id == taskID);
+            task.IsDeleted = true;
+            _context.SaveChanges();
+            resetTasks();
+            return RedirectToAction("ToDOIndex");
+        }
+
+        public IActionResult EditTask(int taskID)
+        {
+            ToDo task = _context.ToDos.FirstOrDefault(t => t.Id == taskID);
+            return View("ToDOIndex", task);
         }
     }
 }
