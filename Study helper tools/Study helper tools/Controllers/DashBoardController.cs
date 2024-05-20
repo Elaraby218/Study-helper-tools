aa1f1ed3ebdfb6f9f836423da39f6d316c42d748
Elaraby218 <152742814+Elaraby218@users.noreply.github.com>
2024-05-20 10:40:27 +0300

    Dashboard conroller added to serve dashboard page

---
+++ Study helper tools/Study helper tools/Controllers/DashBoardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Study_helper_tools.Controllers
+{
+	public class DashBoardController : Controller
+	{
+		public IActionResult DashBoardIndex()
+		{
+			return View();
+		}
+	}
+}
