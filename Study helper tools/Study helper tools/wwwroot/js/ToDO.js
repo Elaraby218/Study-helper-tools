7db0a6c2db82b1408d4d19c236bb8f7cce963a24
Elaraby218 <152742814+Elaraby218@users.noreply.github.com>
2024-05-20 19:44:48 +0300

    add validation functions

--- Study helper tools/Study helper tools/wwwroot/js/ToDO.js
+++ Study helper tools/Study helper tools/wwwroot/js/ToDO.js
@@ -1,1 +1,94 @@
-﻿
+﻿let taskTitle = document.getElementById("taskTitle");
+let taskDescription = document.getElementById("taskDescription");
+/////////////////////////// show and hide validation tip
+
+function showValidationTip(message) {
+    const validationTip = document.getElementById("validationTip");
+    validationTip.textContent = message;
+    validationTip.style.display = "block";
+}
+
+function hideValidationTip() {
+    const validationTip = document.getElementById("validationTip");
+    validationTip.textContent = "";
+    validationTip.style.display = "none";
+}
+
+//////////////////////////////////////////////////////
+
+let taskTitleValidation = function () {
+    let taskTitleValue = taskTitle.value;
+    let regex = /^[a-zA-Z0-9\s]{1,50}$/;
+
+    if (taskTitleValue === "") {
+        return "Task title cannot be empty";
+    } else if (!regex.test(taskTitleValue)) {
+        return "Task title can only contain letters, digits, and spaces";
+    } else {
+        return "";
+    }
+};
+
+
+//////////////////////////////////////////////////////
+
+let taskDescriptionValidation = function () {
+let taskDescriptionValue = taskDescription.value;
+        let regex = /^[a-zA-Z0-9\s]{1,200}$/;
+    
+        if (taskDescriptionValue == "") 
+        {
+        return "Task description cannot be empty" ;
+        } else if (!regex.test(taskDescriptionValue)) {
+    return "Task description can only contain letters, digits, and spaces" ;
+        } else {
+            return ""; 
+        }
+}
+
+
+let validationFunctions = [
+    { func: taskTitleValidation, element: taskTitle },
+    { func: taskDescriptionValidation, element: taskDescription },
+];
+
+document.getElementById('uploadForm').onsubmit = function (event) {
+    let isValid = true;
+
+    for (let { func, element } of validationFunctions) {
+        let errorMessage = func();
+        if (errorMessage) {
+            showValidationTip(errorMessage);
+            isValid = false;
+            element.focus();
+            break;
+        }
+    }
+
+    if (!isValid) {
+        event.preventDefault();
+    } else {
+        return;
+    }
+};
+
+// Attach onblur and onmouseout events to input fields
+validationFunctions.forEach(({ func, element }) => {
+    element.addEventListener("blur", function () {
+        let errorMessage = func();
+        if (errorMessage) {
+            showValidationTip(errorMessage);
+        } else {
+            hideValidationTip();
+        }
+    });
+    element.addEventListener("mouseout", function () {
+        let errorMessage = func();
+        if (errorMessage) {
+            showValidationTip(errorMessage);
+        } else {
+            hideValidationTip();
+        }
+    });
+});
+
