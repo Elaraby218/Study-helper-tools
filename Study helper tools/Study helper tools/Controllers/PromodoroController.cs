6bb3de910f15031906dcffa8a0d42e1ef40654c1
Elaraby218 <152742814+Elaraby218@users.noreply.github.com>
2024-05-21 16:50:09 +0300

    - select task actoni added - mark as done task added - edit time per task added

--- Study helper tools/Study helper tools/Controllers/PromodoroController.cs
+++ Study helper tools/Study helper tools/Controllers/PromodoroController.cs
@@ -1,14 +1,51 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Study_helper_tools.Database;
 using Study_helper_tools.Models;
 
 namespace Study_helper_tools.Controllers
 {
     public class PromodoroController : Controller
     {
+        private readonly appDbContext _db;
+        public PromodoroController(appDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult PromodoroIndex()
         {
             SharedValues.setActive("Promdoro"); 
+            SharedValues.CurTask = null;
             return View();
         }
+
+        [HttpPost]
+        public IActionResult SelectTask(int taskId)
+        {
+            // not send it in Model because i need it in another action
+            SharedValues.CurTask = SharedValues.CurUserTasks.Find(x => x.Id == taskId);
+            return View("PromodoroIndex");
+        }
+
+        [HttpPost]
+        public IActionResult MarkDoneTask(int taskId)
+        {
+            ToDo task = _db.ToDos.FirstOrDefault(t=>t.Id == taskId);
+            task.IsDone = true;
+            _db.SaveChanges();
+            SharedValues.CurUserTasks = _db.ToDos.Where(t=>t.UserId == SharedValues.CurUser.Id && t.IsDeleted==false).ToList();
+            SharedValues.setTasks();
+            SharedValues.CurTask = null;
+            return View("PromodoroIndex");
+        }
+
+        [HttpPost]
+        public IActionResult EditTimePerTask(int taskID, int timePerTask)
+        {
+            _db.ToDos.FirstOrDefault(t=>t.Id == taskID).TimePerTask += timePerTask;
+            _db.SaveChanges();
+            return View("PromodoroIndex");
+        }
     }
+
+    
 }
