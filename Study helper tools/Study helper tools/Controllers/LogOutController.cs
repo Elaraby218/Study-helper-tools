7da16aa10b0e9fe55c4db177a6ca393a75c853fc
Elaraby218 <152742814+Elaraby218@users.noreply.github.com>
2024-05-21 11:46:05 +0300

    log out controller that has an action to serve when user want to logout

---
+++ Study helper tools/Study helper tools/Controllers/LogOutController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Study_helper_tools.Models;
+
+namespace Study_helper_tools.Controllers
+{
+    public class LogOutController : Controller
+    {
+        public IActionResult LogOutIndex()
+        {
+            SharedValues.setAllNull();
+            return RedirectToAction("Index", "SignUpLogin");
+        }
+    }
+}
